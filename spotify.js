e6f904a57b3094f3e87b3d17bc0cf067f8e325f0
Atton <jabusau@gmail.com>
2018-08-17 15:52:01 -0700

    combine with azzy

---
+++ spotify.js
@@ -0,0 +1,42 @@
+window.onSpotifyWebPlaybackSDKReady = () => {
+    console.log(token);
+    const player = new Spotify.Player({
+    name: 'Test Player',
+    getOAuthToken: cb => { cb(token); }
+});
+
+// Error handling
+player.addListener('initialization_error', ({ message }) => { console.error(message); });
+player.addListener('authentication_error', ({ message }) => { console.error(message); });
+player.addListener('account_error', ({ message }) => { console.error(message); });
+player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+// Playback status updates
+player.addListener('player_state_changed', state => { console.log(state); });
+
+// Ready
+  player.addListener('ready', ({ device_id }) => {
+    console.log('Ready with Device ID', device_id);
+});
+
+// Not Ready
+  player.addListener('not_ready', ({ device_id }) => {
+    console.log('Device ID has gone offline', device_id);
+ });
+
+ // Connect to the player!
+    player.connect();
+
+    $('#toggleplay').on("click", function() {
+        player.togglePlay();
+    });
+
+    $('#forward').on("click", function() {
+        player.previousTrack();
+    });
+
+    $('#back').on("click", function() {
+        player.nextTrack();
+    });
+
+};
