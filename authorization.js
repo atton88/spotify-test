5d56cf147e2270379234f19bb0e989fd8fcac7e1
Atton <jabusau@gmail.com>
2018-08-17 15:59:28 -0700

    changed redirect url

--- authorization.js
+++ authorization.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     var token = "";
     var userID = "";
     var clientID = "b93cd2a896b04db6968176145cd8537f";
-    var redirectURI = "https://atton88.github.io/spotify-test/";
+    var redirectURI = "https://atton88.github.io/spotify-test/player.html";
     var scopeParameter = "&scope=playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private user-read-currently-playing user-modify-playback-state user-read-playback-state app-remote-control streaming user-read-recently-played user-read-birthdate user-read-email user-read-private";
     var queryURL = "https://accounts.spotify.com/authorize/?client_id=" + clientID + "&response_type=token&redirect_uri=" + redirectURI + scopeParameter;
     
