323b1ac60c97060a1ab4893a37f3be79e9fa0ab3
Atton <jabusau@gmail.com>
2018-08-18 15:30:19 -0700

    testing  new page token

--- assets/javascript/authorization.js
+++ assets/javascript/authorization.js
@@ -1,52 +1,25 @@
+
+
 function authorizeSpotify() {
     var token = "";
     var userID = "";
     var clientID = "b93cd2a896b04db6968176145cd8537f";
-    var redirectURI = "https://atton88.github.io/spotify-test/";
+    var redirectURI = "https://atton88.github.io/spotify-test/player.html";
     var scopeParameter = "&scope=playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private user-read-currently-playing user-modify-playback-state user-read-playback-state app-remote-control streaming user-read-recently-played user-read-birthdate user-read-email user-read-private";
     var queryURL = "https://accounts.spotify.com/authorize/?client_id=" + clientID + "&response_type=token&redirect_uri=" + redirectURI + scopeParameter;
     
     console.log("current url: " + window.location.href);
     // var teststr = "https://atton88.github.io/spotify-test/#access_token=BQAdcq499rKKgiIIMgbS4bercAR_HZWgsAIiqg1q4hb2DUHbKgqY_CKnfNvayC83gPsjiBDmE3rSVQQzBZ5tSW66eiP7yT9J2ca9UIqK2rsB_HP5DOqOfWohieiA6liFvfHfLkhZeOMs5g&token_type=Bearer&expires_in=3600";
     window.open(queryURL,"_self");
-
 }
-  //parse url, returns token
-  function parseURL(str) {
-    console.log(str);
-    str = str.split("#access_token=");
-    str = str[1].split("&token_type");
-   console.log(str[0]);
-   return str[0];
-  }
 
-function getUserInfo () {
-  queryURL = "https://api.spotify.com/v1/me";
-  user = "124009025";
-
-  $.ajax({
-      url: queryURL,
-      headers: {
-          'Authorization' : 'Bearer ' + token,
-      },
-          method: "GET"
-          }).then(function(response) {
-          console.log("response");
-          userID = response.id;  //get user ID
-          console.log(response);
-          console.log(userID);
-          })
-          return userID;
-  }
-
-  if (window.location.href.includes("access_token")) {
-    token = parseURL(window.location.href);
-    console.log("parsed token: " + token);
-}
 // authorize spotify
 $("#spotifyLoginBtn").on("click", function(){
   authorizeSpotify();
 })
+
+
+
 
 
 
