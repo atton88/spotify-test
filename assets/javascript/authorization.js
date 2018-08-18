f2e33797c7de447bad3e47ac353b509d6fe8d470
Atton <jabusau@gmail.com>
2018-08-17 19:00:39 -0700

    testing token

--- assets/javascript/authorization.js
+++ assets/javascript/authorization.js
@@ -2,17 +2,17 @@ function authorizeSpotify() {
     var token = "";
     var userID = "";
     var clientID = "b93cd2a896b04db6968176145cd8537f";
-    var redirectURI = "https://atton88.github.io/spotify-test/player.html";
+    var redirectURI = "https://atton88.github.io/spotify-test/";
     var scopeParameter = "&scope=playlist-read-collaborative playlist-modify-public playlist-read-private playlist-modify-private user-read-currently-playing user-modify-playback-state user-read-playback-state app-remote-control streaming user-read-recently-played user-read-birthdate user-read-email user-read-private";
     var queryURL = "https://accounts.spotify.com/authorize/?client_id=" + clientID + "&response_type=token&redirect_uri=" + redirectURI + scopeParameter;
     
     console.log("current url: " + window.location.href);
     // var teststr = "https://atton88.github.io/spotify-test/#access_token=BQAdcq499rKKgiIIMgbS4bercAR_HZWgsAIiqg1q4hb2DUHbKgqY_CKnfNvayC83gPsjiBDmE3rSVQQzBZ5tSW66eiP7yT9J2ca9UIqK2rsB_HP5DOqOfWohieiA6liFvfHfLkhZeOMs5g&token_type=Bearer&expires_in=3600";
     window.open(queryURL,"_self");
-  //   if (window.location.href.includes("access_token")) {
-  //   token = parseURL(window.location.href);
-  //   console.log("parsed token: " + token);
-  // }
+    if (window.location.href.includes("access_token")) {
+      token = parseURL(window.location.href);
+      console.log("parsed token: " + token);
+  }
 }
   //parse url, returns token
   function parseURL(str) {
